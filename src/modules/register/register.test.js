2b123b2c5b02103c90da80f50a6fb5b850c8a1d1
Steban Castro <steban.castro@unipamplona.edu.co>
2021-03-29 00:08:37 -0500

    change fetch-node to axios with cookies

--- src/modules/register/register.test.js
+++ src/modules/register/register.test.js
@@ -1,12 +1,21 @@
-const fetch = require('node-fetch');
+/**
+ * @jest-environment node
+ */
+
+const axios = require('axios').default;
+const tough = require('tough-cookie');
+const axiosCookieJarSupport = require('axios-cookiejar-support').default;
 const db = require('../../database');
 const { test } = require('../../database/config');
 const port = process.env.PORT || 3000;
 const host = process.env.FRONTEND_HOST || "localhost";
 const address = `http://${host}:${port}/api`;
+var cookieJar;
 
 beforeAll(async () => {
     await db.sequelize.sync();
+    axiosCookieJarSupport(axios);
+    cookieJar = new tough.CookieJar();
 });
 
 const email = "test@test.com";
@@ -21,24 +30,52 @@ mutation {
 `;
 
 describe("register function", () => {
-    it("case successfully", async () => {
+    it("user create successfully", async () => {
         const response = await postApi(query(email, password));
         const user = await db.User.findOne({ where: { email } });
         expect(response.data.register).toEqual({ id: user.id, email: user.email });
     });
 
-    it("case user already exists", async () => {
-        const response = await postApi(query(email, password));
+    it("user already exists", async () => {
+        const response = await postApi(query(email, password), true);
         expect(response.errors[0].extensions.code).toEqual("USER_EXIST");
+    });
+
+    it("session already exists", async () => {
+        const response = await postApi(query("test2@test.com", password));
+        //console.log(response);
+        expect(response.errors[0].extensions.code).toEqual("ACTIVE_SESSION");
+    });
+
+    it("email invalid", async () => {
+        const response = await postApi(query("", password));
+        expect(
+            response.errors[0]
+            .extensions.error
+            .filter(
+                (item) => item.path == "email").length > 0
+            ).toBe(true);
+    });
+
+    it("password invalid", async () => {
+        const response = await postApi(query("", ""));
+        expect(
+            response.errors[0]
+            .extensions.error
+            .filter(
+                (item) => item.path == "password").length > 0
+            ).toBe(true);
     });
 });
 
-async function postApi(query) {
-    const response = await fetch(address, {
-        method: 'POST', 
-        body: JSON.stringify({ query }),
-        credentials: 'include',
-        headers: { "Content-Type": "application/json" },
+async function postApi(query, notCookies) {
+    const response = await axios({
+        method: 'post',
+        jar: !notCookies ? cookieJar: undefined,
+        withCredentials: !notCookies ? true :  false,
+        url: address,
+        data: { query },
     });
-    return await response.json();
+
+    return response.data;
 }
--- src/modules/tmp/tmp.test.js
+++ src/modules/tmp/tmp.test.js
@@ -1,16 +1,35 @@
-const fetch = require('node-fetch');
+/**
+ * @jest-environment node
+ */
+
+const axios = require('axios').default;
+const tough = require('tough-cookie');
+const axiosCookieJarSupport = require('axios-cookiejar-support').default;
 const port = process.env.PORT || 3000;
 const host = process.env.FRONTEND_HOST || "localhost";
 const address = `http://${host}:${port}/api`;
+var cookieJar;
+
+beforeAll(async () => {
+    axiosCookieJarSupport(axios);
+    cookieJar = new tough.CookieJar();
+});
 
 describe("Hello World!", () => {
     test("make sure it works", async () => {
-        const response = await fetch(address, {
-            method: 'POST', 
-            body: JSON.stringify({ query: `query { hello }` }),
-            headers: { "Content-Type": "application/json" },
-        });
-        const json = await response.json();
+        const json = await postApi('query { hello }', true);
         expect(json.data.hello).toBe('Hello world!');
     });
 })
+
+async function postApi(query, notCookies) {
+    const response = await axios({
+        method: 'post',
+        jar: !notCookies ? cookieJar: undefined,
+        withCredentials: !notCookies ? true :  false,
+        url: address,
+        data: { query },
+    });
+
+    return response.data;
+}
